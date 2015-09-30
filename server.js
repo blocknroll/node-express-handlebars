fae8264ff917ea843946e97752e7bbec2f857f7a
Fred Block <info@fredblock.com>
2015-09-29 19:09:33 -0600

    Update 'server.js' Use handlebars as view template engine. Add 'views/layouts/main.handlebars'.
    modified:   server.js
    new file:   views/index.handlebars
    new file:   views/layouts/main.handlebars

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
+var exphbs  = require('express3-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-  res.send('Hello Worlds!');
+  // res.send('Hello Worlds!');
+  res.render('index');
 });
 
 app.use('/public', express.static('public'));
