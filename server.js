8e4b041adca7b73bc4decc3bf982ed181ee2b539
Fred Block <info@fredblock.com>
2015-09-29 19:33:39 -0600

    Update 'server.js' to add dynamic number. Update 'index.handlebars.
    modified:   server.js
    modified:   views/index.handlebars
    modified:   views/layouts/main.handlebars

--- server.js
+++ server.js
@@ -4,16 +4,25 @@ var exphbs  = require('express3-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+
 app.get('/', function (req, res) {
   // res.send('Hello Worlds!');
-  res.render('index');
+
+  var randomNumber = Math.round( Math.random() * 10 );
+
+  res.render('index', {
+    pickANumber: randomNumber
+  });
 });
+
 
 app.get('/about', function (req, res) {
   res.render('about');
 });
 
+
 app.use('/public', express.static('public'));
+
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port);
