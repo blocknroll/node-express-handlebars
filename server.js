fb8ce02b36b6d713b3fad6fe40888c96b55f1385
Fred Block <info@fredblock.com>
2015-09-29 18:40:39 -0600

    Serve static files from 'public' dir.
    new file:   public/test.html
    modified:   server.js

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ app.get('/', function (req, res) {
   res.send('Hello Worlds!');
 });
 
+app.use('/public', express.static('public'));
+
 var port = Number(process.env.PORT || 5000);
 app.listen(port);
 // app.listen(5000);
